249d687b6bfed20c4be4b49f85f1b21a3a7c5ecb
honghao30 <58649794+honghao30@users.noreply.github.com>
2025-01-03 13:18:12 +0900

    파일 추가

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import { glob } from 'glob';
 import path from 'path';
 
@@ -40,42 +39,6 @@ export default defineConfig({
                 lossless: true,
             },
         }),
-        createHtmlPlugin({
-            minify: false,
-            /**
-             * HTML 포맷팅 설정
-             */
-            prettify: {
-                indent_size: 2,
-                indent_char: ' ',
-                indent_with_tabs: false,
-                preserve_newlines: true,
-                max_preserve_newlines: 2,
-                unformatted: ['pre', 'code'],
-                indent_inner_html: true,
-                wrap_line_length: 0,
-                inline: [],
-                end_with_newline: true
-            }
-        }),
-        {
-            name: 'move-js-to-bottom',
-            transformIndexHtml(html) {
-                const scriptRegex = /<script[^>]*src="([^"]+)"[^>]*>(?:<\/script>)?/g;
-                const scripts = [];
-                let match;
-
-                while ((match = scriptRegex.exec(html)) !== null) {
-                    const script = match[0].endsWith('</script>') 
-                        ? match[0] 
-                        : `${match[0]}</script>`;
-                    scripts.push(script);
-                }
-
-                html = html.replace(scriptRegex, '');
-                return html.replace('</body>', `  ${scripts.join('\n  ')}\n</body>`);
-            }
-        }
     ],
     build: {
         outDir: 'dist-public',
