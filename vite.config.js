dcc1a94e9d59bfa0d881a18013535a8b1523b8dd
choiBom <bchoi@the-51.com>
2025-05-21 19:11:22 +0900

    vite 수정 - 테스트 1
    - output > 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -78,13 +78,13 @@ export default defineConfig({
                     if (assetInfo.name && assetInfo.name.endsWith('.css')) {
                         return 'assets/css/index.css'; // 모든 CSS를 assets/css/index.css로 통합
                     }
-                    if (/\.(png|jpe?g|gif|svg|webp)$/i.test(assetInfo.name)) {
-                        return 'assets/images/[name].[extname]'; // 이미지를 assets/images/ 폴더로 저장
+                    if (/\.(png|jpe?g|gif|svg|webp|webm)$/i.test(assetInfo.name)) {
+                        return 'assets/images/[name][extname]'; // 이미지를 assets/images/ 폴더로 저장
                     }
                     // if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name)) {
                     //     return 'assets/fonts/[name].[hash][extname]'; // 폰트를 assets/fonts/ 폴더로 저장
                     // }
-                    return 'assets/other/[name].[hash][extname]'; // 기타 자산은 assets/other/ 폴더로 저장
+                    return 'assets/other/[name][hash][extname]'; // 기타 자산은 assets/other/ 폴더로 저장
                 },
             },
         },
