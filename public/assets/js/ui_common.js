6a32cf262951c4d26495a5aeaf5d42e0e8df4aee
choiBom <bchoi@the-51.com>
2025-05-29 16:22:17 +0900

    ios 하단 노치바 테스트 15

--- public/assets/js/ui_common.js
+++ public/assets/js/ui_common.js
@@ -671,6 +671,7 @@ document.addEventListener("DOMContentLoaded", () => {
     accordion('.basic-type', 'basic');        
     accordion('.open-type', 'basic');   
     inputSearch('.cmp-input__item--search'); 
+    
     const $body = document.querySelector('body');
-    $body.setAttribute('data-option', '--safe-area-bottom')
+    $body.style.padding = "var(--safe-area-bottom)";
 });
