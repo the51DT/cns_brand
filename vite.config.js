deb6905fed73549bfde2cd26f57983a6dec1e967
choiBom <bchoi@the-51.com>
2025-06-16 14:36:07 +0900

    main - interactive banner 테스트 4

--- vite.config.js
+++ vite.config.js
@@ -79,9 +79,15 @@ export default defineConfig({
                         return 'assets/css/index.css'; // 모든 CSS를 assets/css/index.css로 통합
                     }
                     if (/\.(mp4|webm|mov)$/i.test(assetInfo.name)) {
+                        if (assetInfo.name.includes('src/assets/image/main/')) {
+                            return 'src/assets/image/main/[name][extname]';
+                        }
                         return 'assets/images/[name][extname]'; // 영상
                     }
                     if (/\.(png|jpe?g|gif|svg|webp)$/i.test(assetInfo.name)) {
+                        if (assetInfo.name.includes('src/assets/image/main/')) {
+                            return 'src/assets/image/main/[name][extname]';
+                        }
                         return 'assets/images/[name][extname]'; // 이미지를 assets/images/ 폴더로 저장
                     }
                     // if (/\.(woff2?|eot|ttf|otf)$/i.test(assetInfo.name)) {
