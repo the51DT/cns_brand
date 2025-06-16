7e1764a72d1bfa177b0403aa7384b25d2de6b2b5
choiBom <bchoi@the-51.com>
2025-06-16 15:09:37 +0900

    "main - interactive banner 테스트 5" 원복
    - 파일 확장자 원복

--- public/assets/js/main_interactive_hero.js
+++ public/assets/js/main_interactive_hero.js
@@ -15,19 +15,19 @@ let videoGroup = [];
 let posterGroup = [];
 const videoPaths = {
     pc: [
-        '/assets/image/main/main_interactive_hero_mv_01.mov',
+        '/assets/image/main/main_interactive_hero_mv_01.mp4',
         '/assets/image/main/main_interactive_hero_mv_02.mp4',
         '/assets/image/main/main_interactive_hero_mv_03.mp4',
         '/assets/image/main/main_interactive_hero_mv_04.mp4'
     ],
     tablet: [
-        '/assets/image/main/main_interactive_hero_mv_01_tablet.mov',
+        '/assets/image/main/main_interactive_hero_mv_01_tablet.mp4',
         '/assets/image/main/main_interactive_hero_mv_02_tablet.mp4',
         '/assets/image/main/main_interactive_hero_mv_03_tablet.mp4',
         '/assets/image/main/main_interactive_hero_mv_04_tablet.mp4'
     ],
     mobile: [
-        '/assets/image/main/main_interactive_hero_mv_01_mo.mov',
+        '/assets/image/main/main_interactive_hero_mv_01_mo.mp4',
         '/assets/image/main/main_interactive_hero_mv_02_mo.mp4',
         '/assets/image/main/main_interactive_hero_mv_03_mo.mp4',
         '/assets/image/main/main_interactive_hero_mv_04_mo.mp4'
